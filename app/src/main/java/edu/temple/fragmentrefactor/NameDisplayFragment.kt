f51793a4df9a53dd33f176bfe04d194e856fd339
svtslvtt <tup60276@temple.edu>
2024-10-16 15:46:18 -0400

    instantiated views in NameDisplayFragment layout

--- app/src/main/java/edu/temple/fragmentrefactor/NameDisplayFragment.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/NameDisplayFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -13,18 +17,40 @@ class NameDisplayFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
         }
+
     }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_name_display, container, false)
+
     }
+
+    val displayTextView = findViewById<TextView>(R.id.displayTextView)
+    val nameEditText = findViewById<EditText>(R.id.nameEditText)
+    val changeButton = findViewById<Button>(R.id.changeButton)
+
+    changeButton.setOnClickListener {
+        val name = nameEditText.text
+
+
+        displayTextView.text = if (name.isNotBlank()) {
+            "Hello, $name!"
+        } else {
+            "Please enter your name"
+        }
+    }
+
 }
